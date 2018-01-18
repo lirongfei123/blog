13dbf7f2ea6b024d3fd0293cb01df3f56f6d4423
rongping.lrp@alibaba-inc.com <rongping.lrp@alibaba-inc.com>
2018-01-18 21:17:05 +0800

    *_

--- linux/系统脚本/git.sh
+++ linux/系统脚本/git.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-TARGET="/D/blog/技术"
-cd ${TARGET}
-git remote -v | grep fetch | awk '{print $2}' | git pull
-git status | grep "nothing to commit" > /dev/null 2>&1
-if [ $? != 0 ]; then
-    echo "提交新的Commit:"${TARGET##*/}_${DATE}
-    git add .
-    git commit -m ${TARGET##*/}_${DATE}
-    git push
-else
-    echo "没有更改:"${TARGET##*/}
-fi
+CheckList=("/D/blog/技术","/D/blog/生活")
+for file in ${CheckList}/*
+do
+    cd ${file}
+    git remote -v | grep fetch | awk '{print $2}' | git pull
+    git status | grep "nothing to commit" > /dev/null 2>&1
+    if [ $? != 0 ]; then
+        echo "提交新的Commit:"${file##*/}_${DATE}
+        git add .
+        git commit -m ${file##*/}_${DATE}
+        git push
+    else
+        echo "没有更改:"${file##*/}
+    fi
+done
