f2d39bb4f26783a1fdce3a3932f3e6f0e49340c3
rongping.lrp@alibaba-inc.com <rongping.lrp@alibaba-inc.com>
2018-02-09 20:54:48 +0800

    technology_

--- linux/scripts/git.sh
+++ linux/scripts/git.sh
@@ -4,13 +4,13 @@ for data in ${CheckList[@]}
 do  
     echo ${data}
     cd ${data}
-    git remote -v | grep fetch | awk '{print $2}' | git pull
+    git remote -v | grep fetch | awk '{print $2}' | git pull origin master
     git status | grep "nothing to commit" > /dev/null 2>&1
     if [ $? != 0 ]; then
         echo "提交新的Commit:"${data##*/}_${DATE}
         git add .
         git commit -m ${data##*/}_${DATE}
-        git push
+        git push origin master
     else
         echo "没有更改:"${data##*/}
     fi
